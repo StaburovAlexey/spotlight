ce751299709183499f63bacdf6cfd52ecd853304
Alexey Staburov <gilbertfrost@yandex.ru>
2026-06-20 04:41:25 +0300

    add login

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,13 +1,19 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 
 import { healthRoutes } from "./routes/health.routes.js";
+import { loginRoutes } from "./routes/login.routes.js";
 
 export function buildApp() {
   const app = fastify({
     logger: true,
   });
-
+  app.register(cookie);
+  
   app.register(healthRoutes, {
+    prefix: "/api",
+  });
+  app.register(loginRoutes, {
     prefix: "/api",
   });
 
---
+++ apps/api/src/routes/login.routes.ts
@@ -0,0 +1,110 @@
+import type { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma.js";
+import { verify } from "argon2";
+import crypto from "node:crypto";
+
+import type { Session, User } from "../generated/prisma/client.js";
+type UserDto = Pick<
+  User,
+  | "id"
+  | "email"
+  | "username"
+  | "displayName"
+  | "status"
+  | "mustChangePassword"
+  | "role"
+>;
+
+const loginSchema = {
+  body: {
+    type: "object",
+    required: ["login", "password"],
+    properties: {
+      login: { type: "string" },
+      password: { type: "string" },
+    },
+  },
+} as const;
+
+interface BodyLogin {
+  login: string;
+  password: string;
+}
+async function findUser(login: BodyLogin["login"]): Promise<User | null> {
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ email: login }, { username: login }],
+    },
+  });
+  return existingUser;
+}
+async function verifyPassword(
+  passwordHash: User["passwordHash"],
+  password: BodyLogin["password"],
+) {
+  const verifyPassword = await verify(passwordHash, password);
+  return verifyPassword;
+}
+function isDisabledUser(status: User["status"]) {
+  return status === "DISABLED";
+}
+
+function generateSessionToken() {
+  const token = crypto.randomBytes(32).toString("hex");
+  const tokenHash = crypto.createHash("sha256").update(token).digest("hex");
+  return { token, tokenHash };
+}
+
+async function createSession(user: User, tokenHash: string): Promise<Session> {
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + 7);
+  const session = await prisma.session.create({
+    data: {
+      tokenHash: tokenHash,
+      userId: user.id,
+      expiresAt: expiresAt,
+    },
+  });
+  return session;
+}
+
+function userDto(user: User): UserDto {
+  const { id, email, username, displayName, role, status, mustChangePassword } =
+    user;
+  return { id, email, username, displayName, role, status, mustChangePassword };
+}
+export async function loginRoutes(app: FastifyInstance) {
+  app.post("/login", { schema: loginSchema }, async (request, reply) => {
+    const { login, password } = request.body as BodyLogin;
+    const user = await findUser(login);
+    if (!user) {
+      return reply
+        .code(401)
+        .send({ success: false, error: "Invalid username or password" });
+    }
+    const verify = await verifyPassword(user.passwordHash, password);
+    if (!verify) {
+      return reply
+        .code(401)
+        .send({ success: false, error: "Invalid username or password" });
+    }
+    if (isDisabledUser(user.status)) {
+      return reply
+        .code(403)
+        .send({ success: false, error: "User is disabled" });
+    }
+    const { token, tokenHash } = generateSessionToken();
+    const session = await createSession(user, tokenHash);
+    reply.setCookie("session", token, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: false,
+      maxAge: 60 * 60 * 24 * 7,
+    });
+    reply.code(200).send({
+      success: true,
+      data: userDto(user),
+    });
+  });
+}
